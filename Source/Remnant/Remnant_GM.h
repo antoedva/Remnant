c5c3db805c9a01986678b595b9fa639806bae6f2
Anton Edvardsson <antoedva@playgroundsquad.com>
2018-09-18 15:41:37 +0200

    Add character and movement BUG: Crouch doesn't work

---
+++ Source/Remnant/Remnant_GM.h
@@ -0,0 +1,21 @@
+// Belongs to Anton Edvardsson, Martin Larsson and Katrine Olsen
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "Remnant_GM.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class REMNANT_API ARemnant_GM : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+public:
+	ARemnant_GM();
+	
+	
+};
