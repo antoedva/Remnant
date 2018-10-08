e3359ca633ff14b51316747696dea7516c5c3124
Martin Larsson <martlars@playgroundsquad.com>
2018-10-08 14:25:21 +0200

    add inventory pointer to interact component which is sent to the interactactors on interact.

--- Source/Remnant/PuzzleSystem/Components/InteractComponent.cpp
+++ Source/Remnant/PuzzleSystem/Components/InteractComponent.cpp
@@ -4,7 +4,7 @@
 #include "GameFramework/PlayerController.h"
 #include "FP_Character.h"
 #include "PuzzleSystem/Actors/InteractableActorBase.h"
-#include "TimeClock/ClockComponent.h"
+#include "PuzzleSystem/Components/InventoryComponent.h"
 
 #include "DrawDebugHelpers.h"
 
@@ -22,6 +22,8 @@ void UInteractComponent::BeginPlay()
 	{
 		UE_LOG(LogTemp, Error, TEXT("Failed to get owner in InteractComponent."));
 	}
+
+	cachedInventoryComponent = GetOwner()->FindComponentByClass<UInventoryComponent>();
 }
 
 void UInteractComponent::AttemptInteract()
@@ -34,8 +36,14 @@ void UInteractComponent::AttemptInteract()
 		AInteractableActorBase* interactableActor = Cast<AInteractableActorBase>(hitResult.GetActor());
 		if (interactableActor)
 		{
-			interactableActor->InteractWith();
-			return;
+			if (cachedInventoryComponent)
+			{
+				interactableActor->InteractWith(cachedInventoryComponent);
+			}
+			else
+			{
+				UE_LOG(LogTemp, Error, TEXT("variable cachedInventoryComponent in InteractComponent is nullptr!"));
+			}
 		}
 	}
 }
