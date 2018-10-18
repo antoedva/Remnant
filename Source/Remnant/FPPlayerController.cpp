d4b432c01a8cfb3a09c2f7d64661826814aae438
PSQ\mikefilh <mikefilh@playgroundsquad.com>
2018-10-18 15:29:12 +0200

    idefk

--- Source/Remnant/FPPlayerController.cpp
+++ Source/Remnant/FPPlayerController.cpp
@@ -10,14 +10,4 @@ AFPPlayerController::AFPPlayerController()
 void AFPPlayerController::BeginPlay()
 {
 	Super::BeginPlay();
-
-	APlayerController* pc = GetWorld()->GetFirstPlayerController();
-	if (this == pc)
-	{
-		UE_LOG(LogTemp, Warning, TEXT("The same"));
-	}
-	else
-	{
-		UE_LOG(LogTemp, Warning, TEXT("Not the same"));
-	}
 }
