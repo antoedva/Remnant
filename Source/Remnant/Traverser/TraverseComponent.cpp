2b557eafc632548cf7492b65f1de07e19551eea0
Anton Edvardsson <antoedva@playgroundsquad.com>
2018-10-25 08:37:10 +0200

    Use curve reference instead of creating my own in c++ because richcurve randomly crashes

--- Source/Remnant/Traverser/TraverseComponent.cpp
+++ Source/Remnant/Traverser/TraverseComponent.cpp
@@ -86,27 +86,27 @@ void UTraverseComponent::TraverseDimension()
 			if (!stream)
 				continue;
 			// Remove when the clock shader is implemented
-			if (level.Key == LevelID::OBJECT)
-			{
-				// Change visibility on items depending on which dimension is current 
-				for (AActor* actor : stream->GetLoadedLevel()->Actors)
-				{
-					if (!actor)
-						continue;
+			//if (level.Key == LevelID::OBJECT)
+			//{
+			//	// Change visibility on items depending on which dimension is current 
+			//	for (AActor* actor : stream->GetLoadedLevel()->Actors)
+			//	{
+			//		if (!actor)
+			//			continue;
 
-					if (!actor->HasValidRootComponent())
-						continue;
+			//		if (!actor->HasValidRootComponent())
+			//			continue;
 
-					ToggleObjectVisibility(actor);
-				}
-			}
+			//		ToggleObjectVisibility(actor);
+			//	}
+			//}
 			// !
 
 			level_actor_arrays_.Add(level.Key, stream->GetLoadedLevel()->Actors);
 		}
 
 		timeline_.PlayFromStart();
-		SpawnSphere();
+		//SpawnSphere();
 		TraverseShaderStart(past_traverse_shader_);
 		TraverseShaderStart(present_traverse_shader_);
 		first_skipped_ = true;
@@ -250,19 +250,19 @@ void UTraverseComponent::TickComponent(float delta_time, ELevelTick tick_type, F
 
 	timeline_.TickTimeline(delta_time);
 
-	if (sphere_)
-	{
-		if (!UpdateLevelObjects())
-			return;
+	//if (sphere_)
+	//{
+	//	if (!UpdateLevelObjects())
+	//		return;
 
-		// magic_number_ is magical yes, but it's a carefully calculated value that makes the sphere fit inside the level bounds
-		const float max_scale = level_length_ / magic_number_;
-		// Set sphere scale based on timeline position and length
-		const float slope = max_scale / timeline_length_;
-		const float val = slope * timeline_position_;
+	//	// magic_number_ is magical yes, but it's a carefully calculated value that makes the sphere fit inside the level bounds
+	//	const float max_scale = level_length_ / magic_number_;
+	//	// Set sphere scale based on timeline position and length
+	//	const float slope = max_scale / timeline_length_;
+	//	const float val = slope * timeline_position_;
 
-		sphere_->SetActorScale3D(FVector(val));
-	}
+	//	sphere_->SetActorScale3D(FVector(val));
+	//}
 }
 
 void UTraverseComponent::ToggleObjectVisibility(AActor* actor)
@@ -462,14 +462,19 @@ bool UTraverseComponent::ChangeActorCollision(const bool ignore_distance)
 void UTraverseComponent::SetupTimeline()
 {
 	// Set default value here because UPROPERTY seems to mess things up :(
-	if (timeline_length_ == 0.0f)
-		timeline_length_ = 5.0f;
+	//if (timeline_length_ == 0.0f)
+		//timeline_length_ = 5.0f;
+	//curve_ = NewObject<UCurveFloat>();
+	//timeline_.SetTimelineLengthMode(TL_TimelineLength);
+	//timeline_.SetTimelineLength(timeline_length_);
+	//curve_->FloatCurve.AddKey(0.0f, 0.0f);
+	//curve_->FloatCurve.AddKey(timeline_.GetTimelineLength(), level_length_ * 0.5f);
 
-	curve_ = NewObject<UCurveFloat>();
-	timeline_.SetTimelineLengthMode(TL_TimelineLength);
-	timeline_.SetTimelineLength(timeline_length_);
-	curve_->FloatCurve.AddKey(0.0f, 0.0f);
-	curve_->FloatCurve.AddKey(timeline_.GetTimelineLength(), level_length_ * 0.5f);
+	if (!curve_)
+	{
+		UE_LOG(LogTemp, Error, TEXT("Traverse float curve not set!"));
+		return;
+	}
 
 	FOnTimelineFloat tl_cb;
 	FOnTimelineEventStatic tl_end_cb;
@@ -478,6 +483,8 @@ void UTraverseComponent::SetupTimeline()
 	tl_end_cb.BindUFunction(this, FName{ TEXT("TimelineEndCB") });
 	timeline_.AddInterpFloat(curve_, tl_cb);
 	timeline_.SetTimelineFinishedFunc(tl_end_cb);
+
+	timeline_length_ = curve_->GetFloatValue(timeline_.GetTimelineLength());
 }
 
 void UTraverseComponent::TimelineCB()
@@ -490,6 +497,8 @@ void UTraverseComponent::TimelineCB()
 
 	if (!present_traverse_shader_.collection_instance_->SetScalarParameterValue(FName("Distance"), curve_value_))
 		UE_LOG(LogTemp, Warning, TEXT("Failed to find distance paramater"));
+
+	UpdateLevelObjects();
 }
 
 void UTraverseComponent::TimelineEndCB()
@@ -500,12 +509,11 @@ void UTraverseComponent::TimelineEndCB()
 	if (!present_traverse_shader_.collection_instance_->SetScalarParameterValue(FName("Distance"), level_length_))
 		UE_LOG(LogTemp, Warning, TEXT("Failed to find distance paramater"));
 
-
 	ChangeActorCollision(true);
-	if (sphere_)
-	{
-		GetWorld()->DestroyActor(sphere_);
-		sphere_ = nullptr;
-	}
+	//if (sphere_)
+	//{
+	//	GetWorld()->DestroyActor(sphere_);
+	//	sphere_ = nullptr;
+	//}
 }
 
